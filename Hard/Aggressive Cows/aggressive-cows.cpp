f0320f629a54b231f8da00fd51f297697eebe201
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-01-28 00:00:13 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    // void mysort(vector<int> &stalls,int n){
+    //     int s = 0;
+    //     int e = n-1;
+    //     while(s<e){
+    //         swap(stalls[s++],stalls[e--]);
+    //         // stalls[s] = stalls[s] ^ stalls[e];
+    //         // stalls[e] = stalls[s] ^ stalls[e];
+    //         // stalls[s] = stalls[s] ^ stalls[e];
+    //         // s++;
+    //         // e--;
+    //     }
+    // }
+    
+    int ispossible(vector<int> &stalls,int k,int mid){
+        int cowcount = 1;
+        int position = stalls[0];
+        for(int i=1;i<stalls.size();i++){
+            if(stalls[i]-position >= mid){
+                cowcount++;
+                position = stalls[i];
+            }
+            if(cowcount == k)return true;
+        }
+        return false;
+    }
+    
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        // Write your code here
+        // mysort(stalls,n);
+        sort(stalls.begin(),stalls.end());
+        int s = 0;
+        int e = stalls[n-1] - stalls[0];
+        int ans = -1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(ispossible(stalls,k,mid)){
+                ans = mid;
+                s = mid+1;
+            }
+            else {
+                e = mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
