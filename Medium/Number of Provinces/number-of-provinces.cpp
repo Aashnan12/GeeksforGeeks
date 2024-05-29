32b08a8e566efd1b2e9713369ae7de057ceae36a
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-30 05:11:29 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Provinces/number-of-provinces.cpp
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -4,46 +4,39 @@ using namespace std;
 
 
 // } Driver Code Ends
-
-#pragma GCC optimize("Ofast")
-static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+//User function Template for C++
 
 class Solution {
-public:
-    void bfs(unordered_map<int,vector<int>> &map,vector<bool> &vis,int src){
-        queue<int> q;
-        q.push(src);
-        vis[src] = true;
-
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            for(int &v : map[u]){
-                if(!vis[v]){
-                    q.push(v);
-                    vis[v] = true;
-                }
+  public:
+    int n;
+    unordered_map<int,vector<int>> map;
+    
+    void dfs(int i,vector<bool> &vis){
+        vis[i] = true;
+        for(int &u : map[i]){
+            if(!vis[u]){
+                dfs(u,vis);
             }
         }
-        return;
     }
-    int numProvinces(vector<vector<int>>& isConnected,int n) {
-        unordered_map<int,vector<int>> map;
-
+    int numProvinces(vector<vector<int>> adj, int V) {
+        n = V;
+        map.clear();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i != j && isConnected[i][j] == 1){
+                if(i!=j && adj[i][j] == 1){
                     map[i].push_back(j);
                     map[j].push_back(i);
                 }
             }
         }
         int pro = 0;
+        
         vector<bool> vis(n+1,false);
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 pro++;
-                bfs(map,vis,i);
+                dfs(i,vis);
             }
         }
         return pro;
