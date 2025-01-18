3c5ecf42e8a06505dd8a78fcae16de28bdec5969
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2025-01-19 00:24:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -6,22 +6,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    // Function to return max value that can be put in knapsack of capacity.
     int t[1001][1001];
-    int solve(vector<int>& wt, vector<int>& val,int W,int n) {
+    int solve(vector<int>& wt,vector<int>& val,int W,int n){
         if(n==0 || W==0){
             return 0;
         }
-        if(t[n][W] != -1){
-            return t[n][W];
+        if(t[n-1][W] != -1){
+            return t[n-1][W];
         }
         if(wt[n-1] <= W){
-            return t[n][W] = max(val[n-1] + solve(wt,val,W-wt[n-1],n-1) , solve(wt,val,W,n-1));
+            return t[n-1][W] = max(val[n-1] + solve(wt,val,W - wt[n-1],n-1), solve(wt,val,W,n-1));
         }
         else if(wt[n-1] > W){
-            return t[n][W] = solve(wt,val,W,n-1);
+            return t[n-1][W] = solve(wt,val,W,n-1);
         }
     }
-    // Function to return max value that can be put in knapsack of capacity.
     int knapSack(int capacity, vector<int> &val, vector<int> &wt) {
         // code here
         memset(t,-1,sizeof(t));
